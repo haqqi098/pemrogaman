ec15f50225714ed9c0329d05bdb754714d1475db
haqqi098 <149638208+haqqi098@users.noreply.github.com>
2023-11-09 11:36:52 +0800

    Add files via upload
    pemrogaman pertama zainul haqqi mr (23241115)

---
+++ Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+        // Membuat Aplikasi Biodata
+        // Deklarasi Variable Dan Tipe Data
+        String nama;
+        String nim;
+        String prodi;
+        String tgl_lahir;
+        float tinggi_badan;
+        double berat_badan;
+        boolean status; // jika menikah = true, belum = false 
+
+         // Deklarasi Variable Dan Tipe Data
+        nama = "zainul haqqi mr";
+        nim = "23241115";
+        prodi = "Pendidikan Teknologi Informasi";
+        tgl_lahir = "04 agustus 2004";
+        tinggi_badan = 160.5f;
+        berat_badan = 55.7d;
+        status = true;
+
+        //Mencetak biodata ke layar komputer
+        System.out.println("====================================");
+        System.out.println("nama mahasiswa : " + nama);
+        System.out.println("nim : " + nim);
+        System.out.println("prodi : " + prodi);
+        System.out.println("tgl lahir : " + tgl_lahir);
+        System.out.println("tinggi badan : " + tinggi_badan);
+        System.out.println("berat badan : " + berat_badan);
+        System.out.println("status : " + status);
+        System.out.println("=====================================");
+
+
+    }
+}
